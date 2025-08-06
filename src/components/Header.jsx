84a9cdff4af4ce924a5867b00422ecca16beeefc
Yasmin Hillis <.com>
2025-08-07 02:49:22 +0300

    add context #3
    Co-authored-by: noorradi4 <noorradi88@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+
+import { ThemeContext } from "../App"
+import { useContext } from "react"
 export default function Header(){
+    const { theme } = useContext(ThemeContext)
     return (
-        <header className="light-theme dark-theme">
-            <h1>Light Theme</h1>
+        <header className={`${theme}-theme`}>
+            <h1>{theme === 'light' ? 'Light' : 'Dark'} Theme</h1>
         </header>
 )
 }
